b3fbb942fe0578f245f9bf5f77b86bde3974ac9d
TienMinh25 <letienminh2512@gmail.com>
2023-06-27 13:13:02 +0700

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,21 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def create
+    #relative to strong parameters
+    @user = User.new(user_params)
+    if @user.save
+      #handle a successful save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to user_url(@user)
+    else
+      render('new', {status: :unprocessable_entity})
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
 end
