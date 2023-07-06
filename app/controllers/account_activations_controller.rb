429bcd75b8c2a591e8116572cd94cea99f49d9cc
Zero Night <119108877+TienMinh25@users.noreply.github.com>
2023-07-06 12:48:56 +0700

    Chapter 12: Password Reset (#15)

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -3,14 +3,14 @@ class AccountActivationsController < ApplicationController
     # email lay tren query parameters
     user = User.find_by email: params[:email]
     # Use params[:id] to pass in here is the same with params[:new_token] -> vi dung :id tren parameter nen dung :id de lay theo hash thoi
-    if user && !user.activated? && user.authenticated?(:activation, params[:id])
-      user.update_attributes(activated: true, activated_at: Time.zone.now)
-      log_in user
+    if (user && !user.activated? && user.authenticated?(:activation, params[:id]))
+      user.update_attributes(:activated: true, :activated_at: Time.zone.now)
+      log_in(user)
       flash[:success] = "Account activated!"
-      redirect_to user_path(user)
+      redirect_to user_path(user) 
     else
       flash[:danger] = "Invalid activation link"
-      redirect_to root_url
+      redirect_to root_url 
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,9 @@ Rails.application.routes.draw do
     get '/login', to: 'sessions#new'
     post '/login', to: 'sessions#create'
     delete '/logout', to: 'sessions#destroy'
+    get 'account_activations/edit_account_activation_url'
     resources :users
     resources :account_activations, only: [:edit]
+    resources :password_resets, only: [:new, :create, :edit, :update]
   end
 end
